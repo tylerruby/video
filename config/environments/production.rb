408a44f0caeadd3e0b715101590ea824cf75888a
tylerruby <tylerruby@comcast.net>
2015-07-09 15:22:19 -0400

    changed host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
 
-Rails.application.routes.default_url_options[:host] = 'gentle-river-1982.herokuapp.com'
+  config.default_host = 'gentle-river-1982.herokuapp.com'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
